93376e7f482a86d87c30f5dad9aa45951d5b1d1a
igor-music <igormusich@gmail.com>
2016-03-19 09:38:44 -0400

    Initial commit

---
+++ WindowsAuthenticationTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Thinktecture.IdentityModel.Client;
+
+namespace rbc.mt.WindowsAuthentication.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var handler = new HttpClientHandler
+            {
+                UseDefaultCredentials = true
+            };
+
+            var oauthClient = new OAuth2Client(
+                new Uri("https://localhost:44350/token"),
+                handler);
+
+            var result = oauthClient.RequestCustomGrantAsync("windows").Result;
+        }
+    }
+}
